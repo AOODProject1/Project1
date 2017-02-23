af5f2b4d0f2080d23f97015fe87f71ac47f28a80
Morgan <quixoticbibliomania@gmail.com>
2017-02-23 16:40:41 -0500

    light changes

--- Project1/src/aoodp1/screens/MainScreen.java
+++ Project1/src/aoodp1/screens/MainScreen.java
@@ -1,20 +1,26 @@
 package aoodp1.screens;
 
-import java.awt.dnd.*;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
-import java.awt.FlowLayout;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSource;
+import java.awt.dnd.DragSourceDragEvent;
+import java.awt.dnd.DragSourceDropEvent;
+import java.awt.dnd.DragSourceEvent;
+import java.awt.dnd.DragSourceListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
-import java.awt.event.WindowAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -23,6 +29,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
+
 import javax.swing.DropMode;
 import javax.swing.JFrame;
 import javax.swing.JList;
@@ -70,7 +77,8 @@ public class MainScreen {
 				public void actionPerformed(ActionEvent e) {
 					compOption = Constants.SORTBYDATE;
 					dateOption = Priority.toPriority((((JMenuItem) e.getSource()).getText()));
-						// changes dateOption to the priority on the JMenuItems' name
+					// changes dateOption to the priority on the JMenuItems'
+					// name
 					sortToDos();
 				}
 			});
@@ -86,7 +94,7 @@ public class MainScreen {
 		sort.add(sd);
 		f.setSize(500, 500);
 		f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		f.addWindowListener(new SaveAtClose());
+		f.addWindowListener(new WindowManage());
 		f.setLayout(new FlowLayout());
 		JMenuBar bar = new JMenuBar();
 		f.setLayout(new BorderLayout());
@@ -117,9 +125,10 @@ public class MainScreen {
 				}
 			}
 		});
-		newInputItem.addMouseListener(new MouseAdapter() { //Clears text field when clicked
+		newInputItem.addMouseListener(new MouseAdapter() { // Clears text field
+															// when clicked
 			public void mousePressed(MouseEvent e) {
-				((JTextField)e.getSource()).setText(null);
+				((JTextField) e.getSource()).setText(null);
 			}
 		});
 		// f.pack();
@@ -150,11 +159,11 @@ public class MainScreen {
 		items.setListData(toDos.toArray(new ActionItem[0]));
 	}
 
-	private static boolean close() {
+	private static void close() {
 		int confirm = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?", "Confirm Quit",
 				JOptionPane.YES_NO_OPTION);
-		if (confirm == JOptionPane.NO_OPTION)
-			return false;
+		if (confirm == JOptionPane.NO_OPTION) // person doesn't want to leave
+			return;
 		if (whereToSave == null)
 			System.exit(0);
 		try {
@@ -168,7 +177,6 @@ public class MainScreen {
 		} catch (IOException e1) {
 		}
 		System.exit(0);
-		return true; // technically unreachable
 	}
 
 	public static int getComparason() {
@@ -179,7 +187,7 @@ public class MainScreen {
 		return dateOption;
 	}
 
-	private static class SaveAtClose extends WindowAdapter {
+	private static class WindowManage extends WindowAdapter {
 		public void windowClosing(WindowEvent e) {
 			close();
 		}
@@ -194,15 +202,7 @@ public class MainScreen {
 
 	private static class SaveListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			// String fileName = JOptionPane.showInputDialog("Input a file
-			// name");
-			// try {
-			// whereToSave = new File(FILEHEADER+fileName+".tdl");
-			// System.out.println(whereToSave);
-			// } catch (NullPointerException
-			// x){System.err.println(x.getMessage());}
 			try {
-				// System.out.println(whereToSave.getAbsolutePath());
 				whereToSave.getParentFile().mkdirs();
 				whereToSave.createNewFile();
 				ObjectOutputStream p = new ObjectOutputStream(new FileOutputStream(whereToSave));
@@ -218,9 +218,8 @@ public class MainScreen {
 
 	private static class QuitListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if (!close()) {
-				return;
-			}
+			close();
+			return;
 
 		}
 
@@ -247,13 +246,16 @@ public class MainScreen {
 
 	private class DragListener implements DragSourceListener, DragGestureListener {
 		private DragSource ds = new DragSource();
+
 		public DragListener() {
-		    ds.createDefaultDragGestureRecognizer(items,DnDConstants.ACTION_MOVE, this);
+			ds.createDefaultDragGestureRecognizer(items, DnDConstants.ACTION_MOVE, this);
 		}
-		  public void dragGestureRecognized(DragGestureEvent dge) {
-			    StringSelection transferable = new StringSelection(Integer.toString(items.getSelectedIndex()));
-			    ds.startDrag(dge, DragSource.DefaultCopyDrop, transferable, this);
-			  }
+
+		public void dragGestureRecognized(DragGestureEvent dge) {
+			StringSelection transferable = new StringSelection(Integer.toString(items.getSelectedIndex()));
+			ds.startDrag(dge, DragSource.DefaultCopyDrop, transferable, this);
+		}
+
 		public void dragEnter(DragSourceDragEvent dsde) {
 		}
 
