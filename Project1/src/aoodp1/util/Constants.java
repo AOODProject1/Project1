f8b11a9ec23a03e789ecc5b4640fb87102da6afe
Morgan <quixoticbibliomania@gmail.com>
2017-02-23 20:00:12 -0500

    Constants changed to user.home

--- Project1/src/aoodp1/util/Constants.java
+++ Project1/src/aoodp1/util/Constants.java
@@ -1,8 +1,8 @@
 package aoodp1.util;
 
 public final class Constants {
-	public final static String FILEHEADER = "Z:/ToDoList/";
-			//System.getProperty("user.home") + "/Documents/ToDoList/";
+	public final static String FILEHEADER = //"Z:/ToDoList/";
+			System.getProperty("user.home") + "/Documents/ToDoList/";
 	public final static int SORTBYNAME=0;
 	public final static int SORTBYDATE=1;
 	public final static int SORTNOCARES=-1;
