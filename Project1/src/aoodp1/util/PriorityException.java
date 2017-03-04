53b9bdf184c831826014e57acb508921c83446ad
Morgan <quixoticbibliomania@gmail.com>
2017-03-03 22:18:16 -0500

    finally end the warning about serialversionUID

--- Project1/src/aoodp1/util/PriorityException.java
+++ Project1/src/aoodp1/util/PriorityException.java
@@ -1,6 +1,8 @@
 package aoodp1.util;
 
 public class PriorityException extends RuntimeException {
+	private static final long serialVersionUID = -1943718193751791203L;
+
 	public PriorityException(String s) {
 		super(s);
 	}
